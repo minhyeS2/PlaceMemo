c3eabe91d492fd76dc0fbdb67e78cd73fbf70e2c
KimMinhye <alsgp0123@naver.com>
2025-07-22 14:32:09 +0900

    검색 결과 list UI

--- front/src/components/SearchList.jsx
+++ front/src/components/SearchList.jsx
@@ -1,42 +1,45 @@
+import './searchList.css';
+
 import React from 'react'
 import RatingStar from './RatingStar.jsx';
 
-const SearchList = ({keyword, onChangeKeyword, onSearch, markers, onSelect }) => {
+const SearchList = ({ keyword, onChangeKeyword, onSearch, markers, onSelect }) => {
     return (
-        <div>
-            <div className='list-total'>
-                <div className='search-total'>
-                    <input className='search-box'
+        <div className='list-total'>
+            <div className='search-total'>
+                <div className='search-input'>
+                    <input
                         type='text'
                         value={keyword}
                         onChange={onChangeKeyword}
                         placeholder='キーワードを入力してください！'
                     ></input>
-                    <button className='search-btn'
-                        onClick={onSearch}
-                    >Search</button>
                 </div>
-                <div className='list-box'>
-                    {markers.map((marker) => (
-                        <div className='list-info'
-                            key={marker.placeId}
-                            onClick={() => {
-                                console.log("Clicked: ", marker.placeId); // ✅
-                                onSelect(marker.placeId);
-                            }}>
-                            <div className='store-name'>
-                                <span>{marker.name}</span>
-                            </div>
-                            <div className='store-star'>
-                                <div><span>{marker.rating !== undefined ? marker.rating : '評価なし'}<RatingStar rating={marker.rating} /></span></div>
-                                <div><span>{marker.ratingCnt ? `${marker.ratingCnt}件` : ''}</span></div>
-                            </div>
-                            <div className='store-adr'>
-                                <div><span>{marker.address}</span></div>
-                            </div>
+                <button className='search-btn'
+                    onClick={onSearch}
+                >Search</button>
+            </div>
+            <div className='list-box'>
+                {markers.map((marker) => (
+                    <div className='list-info'
+                        key={marker.placeId}
+                        onClick={() => {
+                            console.log("Clicked: ", marker.placeId); // ✅
+                            onSelect(marker.placeId);
+                        }}>
+                        <div className='store-name'>
+                            <span>{marker.name}</span>
                         </div>
-                    ))}
-                </div>
+                        <div className='store-star'>
+                            <div className='rating'><span>{marker.rating !== undefined ? marker.rating : '評価なし'}</span></div>
+                            <RatingStar rating={marker.rating} />
+                            <div className='rating-cnt'><span>{marker.ratingCnt ? `(${marker.ratingCnt}件)` : ''}</span></div>
+                        </div>
+                        <div className='store-adr'>
+                            <div><span>{marker.address}</span></div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
